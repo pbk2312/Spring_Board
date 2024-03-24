06afb9057786cffcdb4cf7f6bf0cf0434645003b
박유한 <pbk2312@naver.com>
2024-03-24 16:35:11 +0900

    ..

--- src/main/java/board/spring_board/MemberController.java
+++ src/main/java/board/spring_board/MemberController.java
@@ -1,13 +1,28 @@
 package board.spring_board;
 
 
+import board.spring_board.dto.MemberDTO;
+import board.spring_board.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Slf4j
-public class MemberController{
+@RequiredArgsConstructor
+public class MemberController {
 
+    private final MemberService memberService;
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute MemberDTO memberDTO) {
+
+        return "/join";
+    }
 
 
 }
