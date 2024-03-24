818d8035cdeb60320496eb26613aabb728d1d5d7
박유한 <pbk2312@naver.com>
2024-03-24 16:19:24 +0900

    ..

---
+++ src/main/java/board/spring_board/MemberController.java
@@ -0,0 +1,13 @@
+package board.spring_board;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class MemberController{
+
+
+
+}
